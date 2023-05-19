7d5e03a2350e1afad76f873f75727888f8fb35c9
Crilluz <CrilleMega@gmail.com>
2023-05-19 17:23:17 +0200

    Added insertionsort, Insert, and working on ksmallest

--- main.pl
+++ main.pl
@@ -1,5 +1,57 @@
-hello_world :-
-    write("Hello world!").
+nsertionSort([], []).
+insertionSort([(Sum, I, J, Set)|Xs], Res):-
+    iSort(Xs, Res2),
+    insert((Sum, I, J, Set), Res2, Res).
+
+%return empty list on basecase-
+insert([], _, []).
+
+%
+insert(
+    (Sum, I, J, Set), 
+    [], 
+    [(Sum, I, J, Set)]
+).
+
+%
+insert(
+    (Sum, I, J, Set), 
+    [(Sum2, I2, J2, Set2)|Lst], 
+    [(Sum, I, J, Set), (Sum2, I2, J2, Set2)|Lst]):-
+        Sum =< Sum2.
+
+%
+insert(
+    (Sum, I, J, Set), 
+    [(Sum2, I2, J2, Set2)|Lst], 
+    [(Sum2, I2, J2, Set2)|Result]):-
+        Sum > Sum2,
+        insert((Sum, I, J, Set), Lst, Result).
+
+
+
+smallestKset(_, 0, _) :-
+    write('There are no sets to pick. :/'), !.
+    
+smallestKset([], _, _) :-
+    write('Empty.'), !.
+
+smallestKset(List, K, Output) :-
+    kSmallest(List, K, Output),
+    write('Sum\ti\tj\tSublist\n'),
+    stringOutput(Output).
+
+writeKSmallest([(Sum, I, J, Lst)| Rest]):-
+    write((Sum, "\t", I, "\t", J, "\t", Lst)),
+   writeKSmallest(Rest).
+
+
+kSmallest([], _, []).
+kSmallest(List, K, Subs):-
+    %todo
+
+
+
 
 %Generera listan från test_1
 list_test1(List) :-
@@ -15,21 +67,13 @@ list_test3([3,2,-4,3,2,-5,-2,2,3,-3,2,-5,6,-2,2,3]).
 
 main :-
     % Test case 1, k = 15
-    smallestKset(List_test1, 15),
     list_test1(List_test1),
+    smallestKset(List_test1, 15),
 
     % Test case 2, k = 6
-    smallestKset(List_test2, 6),
     list_test2(List_test2),
+    smallestKset(List_test2, 6),
 
     % Test case 3, k = 8
+    list_test3(List_test3).   
     smallestKset(List_test3, 8),
-    list_test3(List_test3).
-
-smallestKset(List, K) :-
-    write("Nice").
-    ( K =< 0 ->
-        write('There are no sets to pick. :/')
-    ;
-        %else do stuff
-    ).        
